4a6ced3a8cf36e8008092cd8acd7b4a19bf9f689
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2024-11-29 18:47:03 +0200

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Footer />
     </main>
   );
 };
